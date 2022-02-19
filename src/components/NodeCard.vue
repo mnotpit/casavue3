20a29ff722cdf88e62182f86dfe2249343bf6c20
mtipton <mnotpit@gmail.com>
2022-02-18 21:06:17 -0500

    adding v2 components

---
+++ src/components/NodeCard.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <h5>
+        {{ node.name }} [{{ node.nodeId }}] ({{
+          this.$store.state.currentNodeId
+        }})
+      </h5>
+      <ul class="nav nav-tabs card-header-tabs" role="tablist">
+        <li class="nav-item" role="presentation">
+          <button
+            class="nav-link active"
+            data-bs-toggle="tab"
+            data-bs-target="#items"
+            role="tab"
+          >
+            <i class="fas fa-network-wired" /> Items
+          </button>
+        </li>
+        <li class="nav-item" role="presentation">
+          <button
+            class="nav-link"
+            data-bs-toggle="tab"
+            data-bs-target="#details"
+          >
+            <i class="fas fa-list" /> Details
+          </button>
+        </li>
+        <li class="nav-item" role="presentation">
+          <button class="nav-link" data-bs-toggle="tab" data-bs-target="#tasks">
+            <i class="fas fa-check" /> Tasks
+          </button>
+        </li>
+        <li class="nav-item" role="presentation">
+          <button
+            class="nav-link"
+            data-bs-toggle="tab"
+            data-bs-target="#resources"
+          >
+            <i class="fas fa-paperclip" /> Resources
+          </button>
+        </li>
+        <li class="nav-item" role="presentation">
+          <button
+            class="nav-link"
+            data-bs-toggle="tab"
+            data-bs-target="#history"
+          >
+            <i class="fas fa-clock" /> History
+          </button>
+        </li>
+      </ul>
+    </div>
+
+    <div class="tab-content">
+      <div class="tab-pane active" id="items" role="tabpanel">
+        <NodeChildNodes :childNodes="node.childNodes" />
+      </div>
+      <div class="tab-pane" id="details" role="tabpanel">
+        <NodeDetails :details="node.details" />
+      </div>
+      <div class="tab-pane" id="tasks" role="tabpanel">
+        <NodeTasks :tasks="node.tasks" />
+      </div>
+      <div class="tab-pane" id="resources" role="tabpanel">
+        <NodeResources :resources="node.resources" />
+      </div>
+      <div class="tab-pane" id="history" role="tabpanel">
+        <NodeHistory />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NodeChildNodes from "./features/childnodes/NodeChildNodes";
+import NodeDetails from "./features/details/NodeDetails";
+import NodeTasks from "./features/tasks/NodeTasks";
+import NodeHistory from "./features/history/NodeHistory";
+import NodeResources from "./features/resources/NodeResources";
+
+export default {
+  name: "NodeCard",
+  components: {
+    NodeChildNodes,
+    NodeDetails,
+    NodeTasks,
+    NodeResources,
+    NodeHistory,
+  },
+  props: {
+    node: Object,
+  },
+};
+</script>
---
+++ src/components/features/childnodes/NodeChildNodes.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="list-group list-group-flush">
+    <ChildNodeItem
+      v-for="node in childNodes"
+      :key="node.nodeId"
+      :id="node.nodeId"
+      :name="node.name"
+    />
+    <a
+      href=""
+      class="list-group-item list-group-item-primary d-flex justify-content-center"
+      data-bs-toggle="modal"
+      data-bs-target="#modalAdd"
+      ><i class="fas fa-plus-square fs-4"></i
+    ></a>
+  </div>
+</template>
+
+<script>
+import ChildNodeItem from "./ChildNodeItem";
+
+export default {
+  name: "NodeChildNodes",
+  props: {
+    childNodes: Array,
+  },
+  components: {
+    ChildNodeItem,
+  },
+};
+</script>
